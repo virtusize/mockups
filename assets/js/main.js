9aa6a6216d67464666f2d4905e4572b705dcafeb
Andrej Babic <andrej@virtusize.com>
2013-11-13 11:06:27 +0100

    Feature: added intro compare images and possibility to switch between sizes

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,6 +45,24 @@
             $view.html(get_tmpl("tmpl-purchase-history"));
         })
 
+        .on("click", ".compare #notice", function(e) {
+            $(this).hide();
+        })
+
+        .on("click", ".compare .btn-pill", function(e) {
+            var $this = $(this),
+                $left = $(".compare .left"),
+                size = $this.data("size");
+
+            $(".compare .size-item.checked").removeClass("checked");
+            $this.parent().addClass("checked");
+            $left
+                .fadeOut(300, function() {
+                    $left.css("background-image", "url('assets/img/compare-intro-%s.png')".replace("%s", size));
+                })
+                .fadeIn();
+        })
+
 
         // Purchase history
 
