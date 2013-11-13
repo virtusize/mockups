d9c23f4378b638214230e976f6376a4b8c27f78b
Andrej Babic <andrej@virtusize.com>
2013-11-13 10:35:48 +0100

    Feature: added purchase history view

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,18 +18,48 @@
         };
 
     $button.on("click", function(e) {
-        e.preventDefault();
         $overlay.show();
 
         if (first) {
+            // Compare w/o garment
             $view.html(get_tmpl("tmpl-compare-first"));
             first = false;
+        }
+        else {
+            // Compare w/ garment
         }
     });
 
     $widget.find(".close").on("click", function(e) {
-        e.preventDefault();
         $overlay.hide();
     });
+
+
+    /* ---- VIEW SPECIFIC ---- */
+
+    $(document)
+
+        // Compare, w/o garment
+
+        .on("click", ".compare #continue", function(e) {
+            $view.html(get_tmpl("tmpl-purchase-history"));
+        })
+
+
+        // Purchase history
+
+        .on("click", ".ph #button", function(e) {
+            if ($(this).hasClass("active")) {
+                alert("next!");
+            }
+            else {
+                $view.html(get_tmpl("tmpl-compare-first"));
+            }
+        })
+
+        .on("click", ".ph .select", function(e) {
+            $(this).toggleClass("on");
+            $(".ph #button").toggleClass("active", $(".ph .select.on").length > 0);
+        });
 
 })(jQuery);
