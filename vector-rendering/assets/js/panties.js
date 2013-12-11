c517f40ee55a73c835bedfc395e1d10996e2edc8
Robert Svensson <robert@virtusize.com>
2013-12-11 13:44:44 +0100

    Feature: Implemented proper SVG transforms.

--- vector-rendering/assets/js/panties.js
+++ vector-rendering/assets/js/panties.js
@@ -1,54 +1,39 @@
 (function (window, _, Snap) {
-    function scalePts(pts, scale) {
-        var scaledPts = {},
-            scaledPoint;
-
-        _.each(pts, function (pointData, pointName) {
-            scaledPoint = {};
-            _.each(pointData, function (value, key) {
-                scaledPoint[key] = scale * value;
-            });
-            scaledPts[pointName] = scaledPoint;
-        });
-
-        return scaledPts;
-    }
-
-    function pointToPathString(pathCmd, point, tx, ty, mirrorX) {
-        var xCoord = mirrorX ? tx - point.x : tx + point.x,
-            yCoord = ty + point.y;
+    function pointToPathString(pathCmd, point, mirrorX) {
+        var xCoord = mirrorX ? -point.x : point.x,
+            yCoord = point.y;
 
         return pathCmd + xCoord + ',' + yCoord;
     }
 
-    function pantiesPathString(pts, tx, ty) {
+    function pantiesPathString(pts) {
         var pathStringArray = [],
             M = 'M',
             L = 'L',
             Z = 'Z';
 
-        pathStringArray.push(pointToPathString(M, pts.p1, tx, ty, false));
-        pathStringArray.push(pointToPathString(L, pts.p2, tx, ty, false));
-        pathStringArray.push(pointToPathString(L, pts.p3, tx, ty, false));
-        pathStringArray.push(pointToPathString(L, pts.p4, tx, ty, false));
-        pathStringArray.push(pointToPathString(L, pts.p4, tx, ty, true));
-        pathStringArray.push(pointToPathString(L, pts.p3, tx, ty, true));
-        pathStringArray.push(pointToPathString(L, pts.p2, tx, ty, true));
-        pathStringArray.push(pointToPathString(L, pts.p1, tx, ty, true));
+        pathStringArray.push(pointToPathString(M, pts.p1, false));
+        pathStringArray.push(pointToPathString(L, pts.p2, false));
+        pathStringArray.push(pointToPathString(L, pts.p3, false));
+        pathStringArray.push(pointToPathString(L, pts.p4, false));
+        pathStringArray.push(pointToPathString(L, pts.p4, true));
+        pathStringArray.push(pointToPathString(L, pts.p3, true));
+        pathStringArray.push(pointToPathString(L, pts.p2, true));
+        pathStringArray.push(pointToPathString(L, pts.p1, true));
         pathStringArray.push(Z);
 
         return pathStringArray.join('');
     }
 
     /*
-    * The whole approach of passing the center point of the SVG view port to the
-    * rendering is only temporary, until proper coordinate system translation is implemented
-    * Also, the center point for the SVG view port should be computed using the bounding box
+    * The SVG view box center point should be computed using the bounding box
     */
     var svg = Snap('svg.demo'),
         svgCX = parseInt(svg.attr('width')) / 2,
         svgCY = parseInt(svg.attr('height')) / 2,
-        ptsOrig = {
+        scaleFactor = 0.9,
+        theMatrix = new Snap.Matrix().translate(svgCX, svgCY).scale(scaleFactor),
+        pts = {
             p1: {x: 17.5, y: 125},
             p2: {x: 192.5, y: -32.5},
             p3: {x: 175, y: -105},
@@ -69,12 +54,12 @@
             c9M: {x: 11.999999999999996, y: -91.14359353944897},
             s1: {s: 13}
         },
-        pts = scalePts(ptsOrig, 0.9),
-        pantiesShape1 = svg.path(pantiesPathString(pts, svgCX, svgCY));
+        pantiesShape1 = svg.path(pantiesPathString(pts));
 
+    pantiesShape1.transform(theMatrix);
     pantiesShape1.attr({
         stroke: '#00aa00',
         fill: 'none',
-        strokeWidth: 4
+        strokeWidth: 4 / scaleFactor
     });
 })(window, window._, window.Snap);
