ea0e7b68f8da0a1af1863b51cc0fad038b7f154e
Bjorn Ekengren <bjorn@virtusize.com>
2014-03-07 15:57:45 +0100

    Feature: First version of wardrobe mockup.

--- v5/assets/js/main.js
+++ v5/assets/js/main.js
@@ -1,7 +1,12 @@
 
 (function($) {
+
+    /**
+     * Start
+     */
     $('#ph-scroll-container').tinyscrollbar({trackSize: 375});
 
+    //Product compare component
     $('.store .overlay').on('mouseenter', function(){
         $('.compare-symbol').addClass('js-hover')
     });
@@ -13,6 +18,15 @@
     });
     $('.compare-symbol').on('mouseleave', function(){
         $('.store .overlay').removeClass('js-hover')
+    });
+
+    /**
+     * Wardrobe
+     */
+    $('.type-dropdown.dropdown .dropdown-toggle').one('click', function(){
+        setTimeout(function(){
+            $('#wardrobe-dropdown-scroll-container').tinyscrollbar();
+        }, 0);
     });
 
 })(jQuery);
